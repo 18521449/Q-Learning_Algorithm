89549ba113552d49c157e9eb51be49b38f8ce6a2
18521449 <80138548+18521449@users.noreply.github.com>
2021-06-05 21:07:27 +0700

    Add files via upload

---
+++ Ban1/decoder.v
@@ -0,0 +1,12 @@
+module decoder (out, in, en);
+`include "params.sv"
+parameter WIDTH = ACTIONS_WIDTH;
+
+input en;
+input [WIDTH-1:0] in;
+output [(2**WIDTH)-1:0] out;
+
+assign out = (en) ? 1 << in : 0;
+
+endmodule
+
